e7714be0157616f13af54a665cb03100982f4e34
anmont <anmont@gmail.com>
2015-01-03 13:11:20 +0800

    Initial Checkin

---
+++ evrun/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace evrun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblHasOre = new System.Windows.Forms.Label();
+            this.runTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEnemies = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMinersInSpace = new System.Windows.Forms.Label();
+            this.lblMiningTarget = new System.Windows.Forms.Label();
+            this.lablelminings = new System.Windows.Forms.Label();
+            this.lblAttackDronesInSpace = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(606, 468);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHasOre
+            // 
+            this.lblHasOre.AutoSize = true;
+            this.lblHasOre.Location = new System.Drawing.Point(222, 20);
+            this.lblHasOre.Name = "lblHasOre";
+            this.lblHasOre.Size = new System.Drawing.Size(0, 13);
+            this.lblHasOre.TabIndex = 1;
+            // 
+            // runTimer
+            // 
+            this.runTimer.Interval = 3000;
+            this.runTimer.Tick += new System.EventHandler(this.runTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enemies";
+            // 
+            // lblEnemies
+            // 
+            this.lblEnemies.AutoSize = true;
+            this.lblEnemies.Location = new System.Drawing.Point(96, 35);
+            this.lblEnemies.Name = "lblEnemies";
+            this.lblEnemies.Size = new System.Drawing.Size(0, 13);
+            this.lblEnemies.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Miners In Space";
+            // 
+            // lblMinersInSpace
+            // 
+            this.lblMinersInSpace.AutoSize = true;
+            this.lblMinersInSpace.Location = new System.Drawing.Point(99, 52);
+            this.lblMinersInSpace.Name = "lblMinersInSpace";
+            this.lblMinersInSpace.Size = new System.Drawing.Size(0, 13);
+            this.lblMinersInSpace.TabIndex = 5;
+            // 
+            // lblMiningTarget
+            // 
+            this.lblMiningTarget.AutoSize = true;
+            this.lblMiningTarget.Location = new System.Drawing.Point(113, 91);
+            this.lblMiningTarget.Name = "lblMiningTarget";
+            this.lblMiningTarget.Size = new System.Drawing.Size(35, 13);
+            this.lblMiningTarget.TabIndex = 9;
+            this.lblMiningTarget.Text = "label5";
+            // 
+            // lablelminings
+            // 
+            this.lablelminings.AutoSize = true;
+            this.lablelminings.Location = new System.Drawing.Point(13, 91);
+            this.lablelminings.Name = "lablelminings";
+            this.lablelminings.Size = new System.Drawing.Size(94, 13);
+            this.lablelminings.TabIndex = 8;
+            this.lablelminings.Text = "Has Mining Target";
+            // 
+            // lblAttackDronesInSpace
+            // 
+            this.lblAttackDronesInSpace.AutoSize = true;
+            this.lblAttackDronesInSpace.Location = new System.Drawing.Point(113, 74);
+            this.lblAttackDronesInSpace.Name = "lblAttackDronesInSpace";
+            this.lblAttackDronesInSpace.Size = new System.Drawing.Size(35, 13);
+            this.lblAttackDronesInSpace.TabIndex = 7;
+            this.lblAttackDronesInSpace.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Attack Drones Out";
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(6, 121);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.Size = new System.Drawing.Size(665, 329);
+            this.rtbMessages.TabIndex = 10;
+            this.rtbMessages.Text = "";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(510, 468);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 503);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.rtbMessages);
+            this.Controls.Add(this.lblMiningTarget);
+            this.Controls.Add(this.lablelminings);
+            this.Controls.Add(this.lblAttackDronesInSpace);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblMinersInSpace);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblEnemies);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHasOre);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblHasOre;
+        private System.Windows.Forms.Timer runTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEnemies;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMinersInSpace;
+        private System.Windows.Forms.Label lblMiningTarget;
+        private System.Windows.Forms.Label lablelminings;
+        private System.Windows.Forms.Label lblAttackDronesInSpace;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ evrun/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace evrun
+{
+    public partial class Form1 : Form
+    {
+        Thread oreChecker;
+        Thread enemyChecker;
+        Thread actionThread;
+        Thread workerChecker;
+        Thread targetChecker;
+
+        public static bool enemies = false;
+        public static int enemieGoneCnt = 0;
+        public static int haveEnemiesCnt = 0;
+        public static int miningTargetCnt = 0;
+        public static List<string> actions = new List<string>();
+        public static bool checkingOre = false;
+        public static bool minersInSpace = false;
+        public static bool attackInSpace = false;
+        public static bool hasMiningTarget = false;
+        public static string log = "";
+        public static bool runBot = false;
+        public static bool exitApp = false;
+        public static bool updatingWorkers = false;
+        public static bool lastMiningStat = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void droneManager()
+        {
+            while (!exitApp)
+            {
+                bool mining = true;
+                //should mining drone be out or attack drone?
+                if (enemies)
+                    mining = false;
+                else
+                {
+                    mining = true;
+                }
+
+                if (lastMiningStat == mining)
+                {
+                    //rest and return
+                    Thread.Sleep(3000);
+                }
+                else
+                {
+                    lastMiningStat = mining;
+
+                    //determine reality
+                    if (RunTest("checkMinerDrones"))
+                    {
+                        minersInSpace = true;
+                    }
+
+                    //match mode
+                    while (mining != minersInSpace)
+                    {
+                        addAction("returnAllDrones");
+                        //while (
+                    }
+
+                    //recall drones
+                }
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            workerChecker = new System.Threading.Thread(new System.Threading.ThreadStart(updateWorkers));
+            workerChecker.Start();
+
+            runBot = true;
+            runTimer.Enabled = true;
+            
+            //lblHasOre.Text = RunTest("ore_full").ToString();
+            //run();
+            //test(@"C:\Users\anmont1\Desktop\sikuli\eve.sikuli\1417585484011.png");
+        }
+
+        public void run()
+        {
+            if (runBot == false)
+            {
+                return;
+            }
+            lblHasOre.Text = "Running";
+            lblEnemies.Text = enemies.ToString();
+            lblMinersInSpace.Text = minersInSpace.ToString();
+            lblAttackDronesInSpace.Text = attackInSpace.ToString();
+            lblMiningTarget.Text = hasMiningTarget.ToString();
+            
+            //handle ore
+
+            
+
+            //check for enemies
+            //checkEnemies();
+
+
+
+            //updateworkers
+            if (minersInSpace == false && attackInSpace == false)
+            {
+                workerChecker = new System.Threading.Thread(new System.Threading.ThreadStart(updateWorkers));
+                workerChecker.Start();
+            }
+
+            if (minersInSpace == true && attackInSpace == true)
+            {
+                workerChecker = new System.Threading.Thread(new System.Threading.ThreadStart(updateWorkers));
+                workerChecker.Start();
+            }
+
+            //if no enemies and attack is in space... bring them back in
+            if (enemies == false && attackInSpace == true)
+            {
+                addAction("returnAllDrones");
+                attackInSpace = false;
+            }
+
+            if (enemies == false && attackInSpace == false && minersInSpace == false)
+            {
+                bool actionExists = false;
+
+                foreach (string act in actions)
+                {
+                    if (act == "deployMiners")
+                    {
+                        actionExists = true;
+                    }
+                }
+
+                if (!actionExists)
+                {
+                    addAction("deployMiners");
+                }
+                
+            }
+
+            if (enemies == false && minersInSpace == true)
+            {
+                if (miningTargetCnt > 3)
+                {
+                    miningTargetCnt = 0;
+                    targetChecker = new System.Threading.Thread(new System.Threading.ThreadStart(checkTarget));
+                    targetChecker.Start();
+                }
+                else
+                {
+                    miningTargetCnt++;
+                }
+            }
+            //if (
+
+            //lblHasOre.Text = "Done";
+            rtbMessages.Text = log;
+        }
+
+        public void checkTarget()
+        {
+            //check idle workers
+
+            addLog("Run Checking Targets");
+            if (RunTest("idleWorkers"))
+            {
+                if(RunTest("hasMiningTarget"))
+                {
+                    //get workers back to work
+                    addLog("Inactive worker Detected");
+                    hasMiningTarget = true;
+                    addAction("returnToWork");
+                }
+                else
+                {
+                    addLog("You have no active target");
+                    hasMiningTarget = false;
+                    //MessageBox.Show("Fighting Done");
+                    System.Media.SystemSounds.Asterisk.Play();
+                    Thread.Sleep(500);
+                    System.Media.SystemSounds.Asterisk.Play();
+                }
+            }
+        }
+
+        public void updateWorkers()
+        {
+            if (!updatingWorkers)
+            {
+                updatingWorkers = true;
+                //new thread
+                addLog("Run Update Workers");
+                if (RunTest("checkMinerDrones"))
+                {
+                    minersInSpace = true;
+                }
+                else
+                {
+                    minersInSpace = false;
+                }
+                if (RunTest("checkAttackDrones"))
+                {
+                    attackInSpace = true;
+                }
+                else
+                {
+                    attackInSpace = false;
+                }
+                addLog("Miners in space = " + minersInSpace.ToString() + " Attack Drones in space = " + attackInSpace.ToString());
+                updatingWorkers = false;
+            }
+        }
+
+        public void checkEnemies()
+        {
+            while (!exitApp)
+            {
+                if (runBot)
+                {
+                    //addLog("Run Check Enemies");
+                    if (RunTest("check_enemy"))
+                    {
+                        if (haveEnemiesCnt > 1)
+                        {
+                            if (enemies == false)
+                            {
+                                addLog("You have a new enemy");
+                                //we ahve a state change
+                                addAction("returnAllDrones");
+                                minersInSpace = false;
+                                addAction("deployAttack");
+                                attackInSpace = true;
+                                //we still need to target and attck
+                            }
+                            enemies = true;
+                            System.Media.SystemSounds.Beep.Play();
+                            enemieGoneCnt = 0;
+                        }
+                        else
+                        {
+                            haveEnemiesCnt++;
+                        }
+
+
+                    }
+                    else
+                    {
+                        if (enemieGoneCnt > 4)
+                        {
+                            //addLog("Your Enemy appears to be gone");
+                            enemies = false;
+                            enemieGoneCnt = 0;
+                            haveEnemiesCnt = 0;
+                        }
+                        else
+                        {
+                            enemieGoneCnt++;
+                        }
+                    }
+                }
+                Thread.Sleep(4000);
+            }
+
+        }
+
+        public void handleOre()
+        {
+            while (!exitApp)
+            {
+                if (runBot)
+                {
+                    //addLog("Run handle ore");
+                    if (!checkingOre)
+                    {
+                        //wrapping checking ore so we arent spamming too many instances
+                        checkingOre = true;
+                        if (RunTest("ore_full"))
+                        {
+                            //ore is full... empty it.
+                            addLog("Your ore is full = " + checkingOre.ToString());
+                            addAction("empty_ore");
+                        }
+                        checkingOre = false;
+                    }
+                }
+                Thread.Sleep(3000);
+            }
+            
+        }
+
+        public static bool RunTest(string sikuliTestName)
+        {
+            int exitCode;
+            //var processInfo = new ProcessStartInfo(@"C:\Users\anmont1\Desktop\sikuli\1_1\runsikulix.cmd", "-r " + @"c:\Users\anmont1\Desktop\sikuli\eve\" + sikuliTestName + ".sikuli");
+            var processInfo = new ProcessStartInfo(@"java ", "-jar " + @"C:\Users\anmont1\Desktop\sikuli\1_1\sikulix.jar -r c:\Users\anmont1\Desktop\sikuli\eve\" + sikuliTestName + ".sikuli");
+
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            Process process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            exitCode = process.ExitCode;
+            process.Close();
+
+            if (exitCode == 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void runTimer_Tick(object sender, EventArgs e)
+        {
+            run();
+        }
+
+        public void addAction(string performAction)
+        {
+            bool dontAdd = false;
+            if (actions.Count > 0)
+            {
+                foreach (string action in actions)
+                {
+                    if (performAction == action)
+                    {
+                        dontAdd = true;
+                    }
+                }
+            }
+
+            if (dontAdd == false)
+            {
+                actions.Add(performAction);
+            }
+
+        }
+
+        public static void takeAction()
+        {
+            while (2 > 1)
+            {
+                if (actions.Count > 0)
+                {
+                    if (!RunTest(actions[0]))
+                    {
+                        if (enemies == true && actions[0] == "deployMiners")
+                        {
+                            minersInSpace = false;
+                        }
+                        else
+                        {
+                            addLog("TAKE ACTION: " + actions[0]);
+                            RunTest(actions[0]);
+                        }
+                    }
+                    actions.RemoveAt(0);
+
+                }
+                Thread.Sleep(500);
+            }
+
+        }
+
+        public static void addLog(string message)
+        {
+            log = System.DateTime.Now.ToLongTimeString() + " " + message + Environment.NewLine + log;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            actionThread = new System.Threading.Thread(new System.Threading.ThreadStart(takeAction));
+            actionThread.Start();
+            oreChecker = new System.Threading.Thread(new System.Threading.ThreadStart(handleOre));
+            oreChecker.Start();
+            enemyChecker = new System.Threading.Thread(new System.Threading.ThreadStart(checkEnemies));
+            enemyChecker.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            runBot = false;
+        }
+    }
+}
+
+
